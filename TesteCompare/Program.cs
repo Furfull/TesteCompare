5491b95b05ee625bed4a9bcd37fffea578e27f43
Furfull <levigabrielufrj@gmail.com>
2023-03-20 00:24:09 -0300

    Add project files.

---
+++ TesteCompare/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using KellermanSoftware.CompareNetObjects;
+using TesteCompare.Models;
+
+namespace TesteCompare
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CompareLogic comparelogic = new();
+            comparelogic.Config.MaxDifferences = Int32.MaxValue;
+
+            Customer Levi = new();
+            Levi.Id = 2;
+            Levi.Name = "Levi Gabriel";
+            Levi.Address = "Rio de Janeiro";
+            Customer Gabriel = new();
+            Gabriel.Id = 2;
+            Gabriel.Name = "Levi Gabriel Souza";
+            Gabriel.Address = "Campinas";
+
+            List<Difference> diffs = comparelogic.Compare(Levi, Gabriel).Differences;
+
+            comparelogic.Config.MaxDifferences = Int32.MaxValue;
+
+            foreach (Difference diff in diffs)
+            {
+                Console.WriteLine(diff.PropertyName);
+                Console.WriteLine(diff.Object1);
+                Console.WriteLine(diff.Object2);
+            }
+        }
+    }
+}
